cfe60d968fe69ce7154cf162f63db43de6b2793a
Abed.Khamees <abed.khamees@hundw.com>
2013-05-13 12:13:32 +0300

    Add the change color function

--- colorChanger/colorViewController.h
+++ colorChanger/colorViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface colorViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+- (IBAction)changeColor:(UIButton *)sender;
+
 
 @end
--- colorChanger/colorViewController.m
+++ colorChanger/colorViewController.m
@@ -26,4 +26,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeColor:(UIButton *)sender {
+    
+    int r = arc4random() % 255;
+    
+    int g = arc4random() % 255;
+
+    int b = arc4random() % 255;
+    
+    UIColor *collor = [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0];
+    
+    [self.view setBackgroundColor:collor];
+}
+
 @end
